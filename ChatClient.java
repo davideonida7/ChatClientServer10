da927bfab8b509fb81f9787fb5caf8d9b05f72ac
davideonida7 <156679005+davideonida7@users.noreply.github.com>
2024-03-27 10:23:10 +0100

    ChatClient.java

---
+++ ChatClient.java
@@ -0,0 +1,28 @@
+class ChatClient {
+    private int id;
+    private Socket socket;
+    private BufferedReader input;
+    private PrintWriter output;
+
+    public ChatClient(int id) {
+        this.id = id;
+    }
+
+    public void start() {
+        try {
+            socket = new Socket("localhost", 6789);
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream(), true);
+            output.println("Hello from Client " + id);
+
+            String response;
+            while ((response = input.readLine()) != null) {
+                System.out.println("[Client " + id + "]: " + response);
+            }
+
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
