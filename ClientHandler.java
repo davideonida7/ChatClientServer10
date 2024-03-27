376206e746b3f25e2d25f52bf6154394cc629a52
davideonida7 <156679005+davideonida7@users.noreply.github.com>
2024-03-27 10:38:44 +0100

    ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -32,21 +32,4 @@ class ClientHandler implements Runnable {
             }
         }
     }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new ChatServer();
-            }
-        });
-
-        for (int i = 0; i < 10; i++) {
-            final int clientId = i;
-            new Thread(new Runnable() {
-                public void run() {
-                    new ChatClient(clientId).start();
-                }
-            }).start();
-        }
-    }
 }
